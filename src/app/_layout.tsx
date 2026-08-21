02acc905f6487a90d62b428e82797b617d89c068
Chet Hill <chetdhill@gmail.com>
2026-08-21 16:30:13 -0500

    Begin Setup for Social Media mobile application

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    // Use Safe Area Provider to ensure that the app content is rendered within the safe area boundaries of a device.
+    <SafeAreaProvider>
+      {/* Use Safe Area View as well, a component that provides a safe area for the app content */}
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ title: "Feed", headerShown: false }}
+          />
+          <Stack.Screen name="profile" options={{ title: "Profile" }} />
+          <Stack.Screen
+            name="notifications"
+            options={{ title: "Notifications" }}
+          />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
