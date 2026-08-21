172bae9348bfa37c10337ac6ac28b81e03bc1cec
Chet Hill <chetdhill@gmail.com>
2026-08-21 15:53:20 -0500

    React Native Basics

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,34 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text style={styles.title}>Hello,</Text>
+      <Text>Welcome to The Olorun Universe!</Text>
+      {/* Shows a simple opacity touch upon interaction */}
+      <TouchableOpacity onPress={() => alert("Dont get touched")}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+
+      {/* A simple pressable button component */}
+      <Pressable onPress={() => alert("They super pressed")}>
+        <Text>Press Me</Text>
+      </Pressable>
+
+      {/* Displays an image from local source */}
+      <Image
+        source={require("../../assets/images/icon.png")}
+        style={{ width: 200, height: 200 }}
+      />
+
+      {/* Displays an image from remote/online source */}
+      {/* 
+        <Image
+          source={{ uri: "https://example.com/path/to/your/image.png" }}
+          style={{ width: 200, height: 200 }}
+        />
+      */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
